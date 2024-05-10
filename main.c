e655d052dc7b9efdaa140d623e9a713b0011e541
Sunil <84917190+sunil-sharma-999@users.noreply.github.com>
2024-05-10 11:31:22 +0530

    Don't allow to insert control characters

--- main.c
+++ main.c
@@ -200,7 +200,14 @@ char *editorRowsToString(int *buflen)
 void editorSave()
 {
     if (E.filename == NULL)
-        return;
+    {
+        E.filename = editorPrompt("Save as: %s");
+        if (E.filename == NULL)
+        {
+            editorSetStatusMessage("Save aborted");
+            return;
+        }
+    }
     int len;
     char *buf = editorRowsToString(&len);
 
@@ -422,6 +429,49 @@ int getWindowSize(int *rows, int *cols)
 
 // Input
 
+char *editorPrompt(char *prompt)
+{
+    size_t bufsize = 128;
+    char *buf = malloc(bufsize);
+    size_t buflen = 0;
+    buf[0] = '\0';
+    while (1)
+    {
+        editorSetStatusMessage(prompt, buf);
+        editorRefreshScreen();
+        int c = editorReadKey();
+        if (c == DELETE_KEY || c == CTRL_KEY('h') || c == BACKSPACE)
+        {
+            if (buflen != 0)
+                buf[--buflen] = '\0';
+        }
+        else if (c == '\x1b')
+        {
+            editorSetStatusMessage("");
+            free(buf);
+            return NULL;
+        }
+        else if (c == '\r')
+        {
+            if (buflen != 0)
+            {
+                editorSetStatusMessage("");
+                return buf;
+            }
+        }
+        else if (!iscntrl(c) && c < 128)
+        {
+            if (buflen == bufsize - 1)
+            {
+                bufsize *= 2;
+                buf = realloc(buf, bufsize);
+            }
+            buf[buflen++] = c;
+            buf[buflen] = '\0';
+        }
+    }
+}
+
 void editorMoveCursor(int key)
 {
 
@@ -528,7 +578,8 @@ void editorProcessKeypress()
     case '\x1b':
         break;
     default:
-        editorInsertChar(c);
+        if (!iscntrl(c))
+            editorInsertChar(c);
         break;
     }
 
